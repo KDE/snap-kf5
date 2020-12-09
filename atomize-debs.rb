3d3299d5f113c73024f3d6408a95cc9dc2f89289
Jonathan Riddell <jr@jriddell.org>
2020-12-09 15:28:38 +0000

    kf5 wants freeglut but does not seem to pull it in so add manually

--- atomize-debs.rb
+++ atomize-debs.rb
@@ -412,11 +412,11 @@ parts = [nil] + parts
 # though they are not strictly frameworks.
 # Notable exception: ssl (multiple versions available)
 devs = %w[libxml2-dev libxslt-dev liblcms2-dev libpng-dev libexiv2-dev
-          libjpeg-dev freeglut3-dev freeglut3]
+          libjpeg-dev]
 # make sure we have gettext available for l10n use
 devs += %w[gettext]
 # mesa-utils-extra - es2_info useful to debug GL problems.
-runs = %w[mesa-utils-extra]
+runs = %w[mesa-utils-extra freeglut3-dev]
 # GStreamer plugins
 runs += %w[gstreamer1.0-x gstreamer1.0-plugins-base
            gstreamer1.0-pulseaudio gstreamer1.0-plugins-good]
